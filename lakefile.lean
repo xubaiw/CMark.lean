72b2be2c3ec5ef8169c3dd28d398aad0829dc1ff
Henrik Böving <hargonix@gmail.com>
2022-07-20 15:27:42 +0200

    feat: compile with PIC

--- lakefile.lean
+++ lakefile.lean
@@ -15,16 +15,16 @@ def wrapperName := "wrapper"
 def buildDir := defaultBuildDir
 
 def cmarkOTarget (srcName : String) : FileTarget :=
-  let oFile := __dir__ / buildDir / cmarkDir / ⟨ srcName ++ ".o" ⟩ 
+  let oFile := __dir__ / buildDir / cmarkDir / ⟨ srcName ++ ".o" ⟩
   let srcTarget := inputFileTarget <| __dir__ / cmarkDir / ⟨ srcName ++ ".c" ⟩
   fileTargetWithDep oFile srcTarget λ srcFile => do
-    compileO oFile srcFile #["-I", (__dir__ / cmarkDir).toString]
+    compileO oFile srcFile #["-I", (__dir__ / cmarkDir).toString, "-fPIC"]
 
 def wrapperOTarget : FileTarget :=
-  let oFile := __dir__ / buildDir / wrapperDir / ⟨ wrapperName ++ ".o" ⟩ 
+  let oFile := __dir__ / buildDir / wrapperDir / ⟨ wrapperName ++ ".o" ⟩
   let srcTarget := inputFileTarget <| __dir__ / wrapperDir / ⟨ wrapperName ++ ".c" ⟩
   fileTargetWithDep oFile srcTarget λ srcFile => do
-    compileO oFile srcFile #["-I", (← getLeanIncludeDir).toString, "-I", (__dir__ / cmarkDir).toString]
+    compileO oFile srcFile #["-I", (← getLeanIncludeDir).toString, "-I", (__dir__ / cmarkDir).toString, "-fPIC"]
 
 def cmarkTarget : FileTarget :=
   let libFile := __dir__ / buildDir / cmarkDir / "libleancmark.a"
